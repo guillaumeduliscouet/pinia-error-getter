251c56f3d21aeb6b0e31eaaf07d138cf77a092aa
Guillaume Duliscouet <g.duliscouet@ubitransport.com>
2022-02-15 14:31:29 +0100

    init

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
+import VueCompositionAPI from '@vue/composition-api'
+import { createPinia, PiniaVuePlugin } from 'pinia'
 import App from './App.vue'
 
 Vue.config.productionTip = false
 
+Vue.use(VueCompositionAPI)
+Vue.use(PiniaVuePlugin)
+
 new Vue({
-  render: h => h(App),
+    render: (h) => h(App),
+    pinia: createPinia(),
 }).$mount('#app')
